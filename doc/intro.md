2eb725044b7d9a6748209084f2cd728bcd1095cd
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2021-07-31 13:33:07 +0100

    New projects: clojure cli project using the app template
    clojure -X:project/new :template app :name practicalli.stackoverflow-survey

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to practicalli.stackoverflow-survey
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/practicalli/stackoverflow_survey_test.clj
@@ -0,0 +1,7 @@
+(ns practicalli.stackoverflow-survey-test
+  (:require [clojure.test :refer :all]
+            [practicalli.stackoverflow-survey :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
