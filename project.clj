b062abe5f699b5a76f68bb1fd4a09c09b5bb4f1b
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2021-07-31 15:11:32 +0100

    Leiningen project configuration file

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject practicalli.stackoverflow-survey "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [scicloj/tablecloth "6.002"]]
+  :main ^:skip-aot practicalli.stackoverflow-survey
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
